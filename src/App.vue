2b167dfd5890b11573c6f4de21d38c3957ac90fd
devxu <devxu@thoughtworks.com>
2020-08-21 15:50:47 +0800

    [todo-list][jeany]: implement add item to todo list

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,50 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>To Do List</h1>
+        <p>Simple Todo List with adding and filter by different status.</p>
+        <input v-model.trim="todoContent">
+        <button @click="addTodoItem">Add</button>
+        <ul class="todo-list">
+            <TodoItem v-for="(item, index) in todoList" :key="index" :todo-item="item" :index="index"></TodoItem>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoItem from "./components/TodoItem";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {TodoItem},
+        data() {
+            return {
+                todoContent: '',
+                todoList: []
+            }
+        },
+        methods: {
+            addTodoItem() {
+                if (this.todoContent.length > 0) {
+                    this.todoList.push({isChecked: false, text: this.todoContent});
+                    this.todoContent = '';
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .todo-list {
+        text-align: left;
+        margin: 10px 30%;
+    }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>
+        <input type="checkbox" :id="index" v-model="todoItem.isChecked">
+        <label :for="index">{{ todoItem.text }}</label>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: 'TodoItem',
+        props: {
+            index: Number,
+            todoItem: {
+                text: String,
+                isChecked: Boolean
+            },
+        }
+    }
+</script>
